2686f5dfb88d134c22aaacc1099efe0a42cb4be9
gregory <greg2502@gmail.com>
2014-07-03 16:43:41 -0700

    fix api_base_path setting and setup token expiration

--- lib/apartment_ratings/configuration.rb
+++ lib/apartment_ratings/configuration.rb
@@ -2,12 +2,14 @@ module ApartmentRatings
   class Configuration < Hashie::Dash
     DEFAULTS = {
       format: 'json',
-      api_base_path: 'https://services.apartmentratings.com/services/rms/v1/'
+      api_base_path: 'https://services.apartmentratings.com/services/rms/v1/',
+      hours_before_token_expiration: 48
     }.freeze
 
     property :username
     property :password
     property :format, default: DEFAULTS[:format]
     property :api_base_path, default: DEFAULTS[:api_base_path]
+    property :hours_before_token_expiration, default: DEFAULTS[:hours_before_token_expiration]
   end
 end
--- spec/apartment_ratings_spec.rb
+++ spec/apartment_ratings_spec.rb
@@ -14,13 +14,21 @@ describe ApartmentRatings do
       end
     end
 
-    context 'when format is specified' do
+    context 'when defaults are sets' do
       let(:format) { 'format' }
+      let(:api_base_path) { 'https://services.apartmentratings.com/services/rms/v2/' }
+      let(:hours_before_token_expiration) { 72 }
 
-      before { described_class.config.format = format }
+      before do
+        described_class.config.format = format
+        described_class.config.api_base_path = api_base_path
+        described_class.config.hours_before_token_expiration = hours_before_token_expiration
+      end
 
       it 'sets the format' do
         expect(subject.format).to eq format
+        expect(subject.api_base_path).to eq api_base_path
+        expect(subject.hours_before_token_expiration).to eq hours_before_token_expiration
       end
     end
 
@@ -29,6 +37,7 @@ describe ApartmentRatings do
       expect(subject.password).to eq password
       expect(subject.format).to eq subject.class::DEFAULTS[:format]
       expect(subject.api_base_path).to eq subject.class::DEFAULTS[:api_base_path]
+      expect(subject.hours_before_token_expiration).to eq subject.class::DEFAULTS[:hours_before_token_expiration]
     end
   end
 end
